14ae00c391d758ff9af18ecb1c9fd1f1c0438c2e
KennSnow <113938170+KennSnow@users.noreply.github.com>
2022-09-19 15:26:57 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
 //document.getElementById('remove').addEventListener('click',remoteItem);
 document.getElementById('add').addEventListener('click',addItem);
+document.getElementById('save').addEventListener('click',saveInfo(document.getElementById('list')));
+document.getElementById('load').addEventListener('click',loadInfo('KennTestCookie'));
 
 function addItem() {
     var input = document.getElementById('input');
@@ -38,4 +40,25 @@ function addItem() {
 
         input.value = '';
     }
+}
+
+function saveInfo(list) {
+    let cookieName = 'KennTestCookie';
+    var expires = '';
+    var date = new Date();
+    date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+
+    document.cookie = cookieName + '=' + (list || "") + expires + "; path=/";
+}
+
+function loadInfo(named) {
+    var nameEQ = named + "=";
+    var ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length;i++){
+        var c = ca[i];
+        while(c.charAt(0) == ' ')c = c.substring(1,c.length);
+        if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
